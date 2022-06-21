5c4054be0e2e6e7cdc4ca06fe18e6fcc5b5e46a7
Oleksii Kurkin <kurkio01@kits.kfplc.com>
2022-06-22 00:56:09 +0300

    Create custom keyboard and typing functionality

--- ArtilleryCalculator/ViewController.swift
+++ ArtilleryCalculator/ViewController.swift
@@ -8,8 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var selectedLabel: UILabel?
+    
+    @IBOutlet weak var srsrSightLabel: UILabel!
+    @IBOutlet weak var natoSightLabel: UILabel!
+    
+    @IBOutlet weak var srsrDegreeMeterFirstLabel: UILabel!
+    @IBOutlet weak var srsrDegreeMeterSecondLabel: UILabel!
+    
+    @IBOutlet weak var natoDegreeMeterFirstLabel: UILabel!
+    @IBOutlet weak var natoDegreeMeterSecondLabel: UILabel!
+    
+    private var labels: [UILabel] {
+        [srsrSightLabel, natoSightLabel,
+         srsrDegreeMeterFirstLabel, srsrDegreeMeterSecondLabel,
+         natoDegreeMeterFirstLabel, natoDegreeMeterSecondLabel]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+        
+        labels.forEach {
+            $0.isUserInteractionEnabled = true
+            let tap = UITapGestureRecognizer(target: self, action: #selector(labelPressed(sender:)))
+            tap.cancelsTouchesInView = false
+            
+            if $0 != natoSightLabel {
+                $0.addGestureRecognizer(tap)
+            }
+        }
+    }
+    
+    @IBAction func colorThemeButtonPressed(_ sender: Any) {
+        print("colorThemeButtonPressed")
+    }
+    
+    @IBAction func refreshButtonPressed(_ sender: Any) {
+        labels.forEach { $0.text = "00" }
+    }
+    
+    @IBAction func countButtonPressed(_ sender: Any) {
+        print("countButtonPressed")
+    }
+    
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+        let newValue = ((selectedLabel?.text == "00" ? "" : selectedLabel?.text) ?? "") + (sender.titleLabel?.text ?? "")
+        selectedLabel?.text = newValue
+    }
+    
+    @objc func labelPressed(sender: UITapGestureRecognizer) {
+        if let labelToSelect = sender.view as? UILabel {
+            selectedLabel = labelToSelect
+        }
     }
 }
